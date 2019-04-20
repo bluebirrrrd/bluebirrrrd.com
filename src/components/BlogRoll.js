61fac38271b7b76b13fc74d42e307fab2831613f
Anna Kurylo <anna.kurilo21@gmail.com>
2019-04-20 21:42:17 +0000

    update styles

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -11,8 +11,8 @@ class BlogRoll extends React.Component {
       <div className="columns is-multiline">
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article className="tile is-child box notification">
+            <div className="is-parent column is-12" key={post.id}>
+              <article className="tile is-child">
                 <p>
                   <Link
                     className="title has-text-primary is-size-4"
