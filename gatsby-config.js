184ff2160084c2d9fba7bad769d3c3ff5d5b911a
Anna Kurylo <anna.kurilo21@gmail.com>
2019-04-23 08:44:28 +0000

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,24 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-71819376-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/static/**"],
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "bluebirrrrd.com",
+      },
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
